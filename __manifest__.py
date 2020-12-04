8a788f71d35d5fdbeb0f8e72596ef2fce0d67d85
Bayu Iskandar <72544381+iskantasia@users.noreply.github.com>
2020-12-04 13:55:01 +0700

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -20,7 +20,12 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'purchase', 'product'],
+    'depends': [
+        'base',
+        'purchase',
+        'product'
+        # 'base64'
+    ],
 
     # always loaded
     'data': [
