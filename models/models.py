71fe92147d00c10729844ccd483c66dcdd9d9270
Bayu Iskandar <72544381+iskantasia@users.noreply.github.com>
2020-12-11 14:07:01 +0700

    Update models.py

--- models/models.py
+++ models/models.py
@@ -3,10 +3,6 @@ from datetime import datetime
 
 from odoo import models, fields, api, _
 from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT
-# from xlrd import open_workbook
-# import base64
-# import xlrd
-# import tempfile
 
 class type_request(models.Model) :
     _name = 'purchasing.type_request'
@@ -21,10 +17,8 @@ class type_request(models.Model) :
 class form_stationery(models.Model):
     _name = 'purchasing.stationery'
     _description = 'purchasing.stationery'
-    # _order = 'date_order desc, id desc'
     _columns = {
         'code_pr': fields.Char(string='PR', help="Auto Generate")
-        # 'code_product': fields.Char(string='CP/', help="Auto Generate")
     }
 
     READONLY_STATES = {
@@ -33,11 +27,8 @@ class form_stationery(models.Model):
         'cancel': [('readonly', True)],
     }
 
-    # import_xls = fields.Binary('File')
     employee_id = fields.Many2one(comodel_name='res.partner', string='Employee')
     company_id = fields.Many2one(comodel_name='res.company', string='Company')
-
-    # company_id = fields.Many2one('res.company', 'Company', required=True, index=True, states=READONLY_STATES, default=lambda self: self.env.company.id)
 
     product_id = fields.One2many(
         comodel_name='product.lines',
@@ -56,113 +47,46 @@ class form_stationery(models.Model):
 
     notes = fields.Text('Terms and Conditions')
     user_id = fields.Many2one('res.users', string=' Purchase Representative')
+    partner_id = fields.Many2one('res.partner', string='Vendor', required=True, states=READONLY_STATES, change_default=True, tracking=True, domain="['|', ('company_id', '=', False), ('company_id', '=', company_id)]", help="You can find a vendor by its Name, TIN, Email or Internal Reference.")
+
     payment_term_id = fields.Many2one('account.payment.term', string='Payment Terms')
     date_planned = fields.Datetime(string='Request Date', index=True)
     product_qty = fields.Many2one('product.lines', string='Quantity', index=True)
     date_from = fields.Datetime(string='From')
     date_to = fields.Date(sting='To')
-    # image = field.Binary(string="Image")
     active = fields.Boolean('Active', default=True)
+    date_order = fields.Datetime(string='Order Date', readonly=True)
 
-    # partner_id = fields.Many2one('res.partner', string='Vendor', required=True, states=READONLY_STATES, change_default=True, tracking=True, domain="['|', ('company_id', '=', False), ('company_id', '=', company_id)]", help="You can find a vendor by its Name, TIN, Email or Internal Reference.")
-    # partner_id = fields.Many2one('res.partner', related='order_id.partner_id', string='Partner', readonly=True, store=True)
-    # dest_address_id = fields.Many2one('res.partner', domain="['|', ('company_id', '=', False), ('company_id', '=', company_id)]", string='Drop Ship Address', states=READONLY_STATES,
-    #     help="Put an address if you want to deliver directly from the vendor to the customer. "
-    #         "Otherwise, keep empty to deliver to your own company.")
-    # currency_id = fields.Many2one(related='order_id.currency_id', store=True, string='Currency', readonly=True)
-    # date_order = fields.Datetime(related='order_id.date_planned', string='Order Date', readonly=True)
+    origin = fields.Char('Source Document', copy=False,
+        help="Reference of the document that generated this purchase order "
+             "request (e.g. a sales order)")
 
-    # amount_untaxed = fields.Monetary(string='Untaxed Amount', store=True, readonly=True, compute='_amount_all', tracking=True)
-    # amount_tax = fields.Monetary(string='Taxes', store=True, readonly=True, compute='_amount_all')
-    # amount_total = fields.Monetary(string='Total', store=True, readonly=True, compute='_amount_all')
+# -------------- compute='_amount_all' ---------------
+    amount_untaxed = fields.Monetary(string='Untaxed Amount', store=True, readonly=True)
+    amount_tax = fields.Monetary(string='Taxes', store=True, readonly=True)
+    amount_total = fields.Monetary(string='Total', store=True, readonly=True)
 
-    # currency_id = fields.Many2one('res.currency', 'Currency', required=True, states=READONLY_STATES, default=lambda self: self.env.company.currency_id.id)
+    price_subtotal = fields.Monetary(string='Subtotal', store=True)
+    # price_total = fields.Monetary(compute='_compute_amount', string='Total', store=True)
+    # price_tax = fields.Float(compute='_compute_amount', string='Tax', store=True)
+
+    invoice_count = fields.Integer(string='Bill Count', copy=False, default=0, store=True)
+    invoice_ids = fields.Many2many('account.move', string='Bills', copy=False, store=True)
+    currency_id = fields.Many2one('res.currency', string='Currency')
+    base_currency_id = fields.Many2one('res.currency', default=lambda self: self.invoice_ids.company_id.currency_id.id)
 
     state = fields.Selection([
-        ('draft', 'RFQ'),   
+        ('draft', 'Purchase Request For Quotation'),   
         ('sent', 'RFQ Sent'),
         ('to approve', 'To Approve'),
-        ('purchase', 'Purchase Order'),
+        ('purchase', 'Purchase Stationery'),
         ('done', 'Locked'),
         ('cancel', 'Cancelled')
     ], string='Status', readonly=True, index=True, copy=False, default='draft', tracking=True)
-    # state = fields.Many2one(comodel_name='purchasing.product_info', string='State', store=True)
-    order_line = fields.One2many('purchase.order.line', 'order_id', string='Order Lines', states={'cancel': [('readonly', True)], 'done': [('readonly', True)]}, copy=True)
 
 #   Generate Code PR Auto
     code_pr = fields.Char(string="PR", required=True, copy=False, readonly=True,
                           index=True, default=lambda self: _('PR'))
-
-    # code_product = fields.Char(string="CP/", required=True, copy=False, readonly=True,
-    #                       index=True, default=lambda self: _('CP/'))
-    # @api.depends('')
-    # def set__group(self):
-    #     for rec in self:
-    #         if rec._:
-    #             if ._ < 18:
-    #                 rec._group = 'min'
-    #             else:
-    #                 rec._group = 'max'
-
-    # def open__(self):
-    #     print("")
-    
-    # def records_import(self, cr, uid, ids, context=None):
-    #     supplier_obj = self.pool.get('res.partner')
-    #     for price in self.browse(cr, uid, ids, context=context):
-    #         fileName, fileExtension = os.path.splitext(price.file_to_import_fname)
-    #         if fileExtension != '.csv':
-    #             raise osv.except_osv(_("Warning !"), _("Change the file type as CSV"))
-    #         price_file = unicode(base64.decodestring(price.import_payment), 'windows-1252', 'strict').split('\n')
-    #         line_count = 0
-    #         ss = len(price_file)
-    #         for line in price_file:
-    #             line_count += 1
-    #             if line_count > 1:
-    #                 if  line_count < ss -1:
-    #                     line_contents = line.split(',')
-    #                     if not line_contents:
-    #                         break
-    #                     Name = line_contents[0].strip()
-    #                     Amount = line_contents[1].strip()
-    #                     supplier_id = supplier_obj.search(cr, uid, [('name', '=', Name)])
-    #                     if supplier_id == []:
-    #                         supplier = {
-    #                             'name':Name
-    #                         }
-    #                         new_supplier = supplier_obj.create(cr, uid, supplier)
-    #                     else:
-    #                         for supp in supplier_obj.browse(cr, uid, supplier_id):
-    #                             new_supplier = supp.id
-    #                     price_line_exist = self.pool.get('sample.lines').search(cr, uid, [('name', '=', new_supplier),('payment_id', '=', price.id),('amount', '=', Amount)])
-    #                     if price_line_exist == []:
-    #                         price_upload_dict = {
-    #                                                 'name' : new_supplier,
-    #                                                 'amount': Amount
-    #                                             }
-    #                         self.pool.get('sample.lines').create(cr, uid, price_upload_dict)
-    #         self.write(cr, uid, price.id,{'upload':True},context=context)
-    #     return {}
-
-    # def import_xls(self):
-    #      wb = xlrd.open_workbook(file_contents=base64.decodestring(self.xls_file))
-    #      for sheet in wb.sheets():
-    #          for row in range(sheet.nrows):
-    #              for col in range(sheet.ncols):
-    #                  print  (sheet.cell(row,col).value)
-
-    # def import_data(self, cr, uid, ids, context=None):
-    #     temp_path = tempfile.gettempdir()
-    #     test_obj = self.pool.get('test.test')
-    #     xls_data = base64.decodestring(xls_file)
-    #     fp=open(temp_path+'/xsl_file.xls', 'wb+')
-    #     fp.write(xls_data)
-    #     fp.close()
-    #     wb = open_workbook(temp_path+'/xsl_file.xls')
-    #     for sheet in wb.sheets():
-    #         for rownum in range(sheet.nrows):
-    #             if rownum == 0:
-    #                 header_list = sheet.row_values(rownum)
 
 #   Function for Generate Code PR
     @api.model
@@ -171,21 +95,7 @@ class form_stationery(models.Model):
 #   Untuk memanggil metode ORM langsung dari sebuah object
             vals['code_pr'] = self.env['ir.sequence'].next_by_code('purchasing.stationery.sequence') or _('PR')
         result = super(form_stationery, self).create(vals)
-
-    # @api.model
-    # def create(self, vals):
-    #     if vals.get('code_product', _('CP/')) == _('CP/'):
-    #         vals['code_product'] = self.env['ir.sequence'].next_by_code('purchasing.stationery.sequence1') or _('CP/')
-    #     result = super(form_stationery, self).create(vals)
         return result
-
-# --------------------------------- Print / Report ---------------------------------
-    # @api.multi
-    # def report(self):
-    #     return self.env.ref('purchasing.stationery.report').report(self)
-    #     self.filtered(lambda s: s.state == 'draft').write({'state' : 'sent'})
-    #     return self.env.ref('purchasing.stationery.report')\
-    #     .with_context({'discard_logo_check': True}).report_action(self)
 
     def action_rfq_send(self):
         '''
@@ -218,9 +128,6 @@ class form_stationery(models.Model):
             'mark_rfq_as_sent': True,
         })
 
-        # In the case of a RFQ or a PO, we want the "View..." button in line with the state of the
-        # object. Therefore, we pass the model description in the context, in the language in which
-        # the template is rendered.
         lang = self.env.context.get('lang')
         if {'default_template_id', 'default_model', 'default_res_id'} <= ctx.keys():
             template = self.env['mail.template'].browse(ctx['default_template_id'])
@@ -244,28 +151,31 @@ class form_stationery(models.Model):
             'context': ctx,
         }
 
+    # @api.onchange('price_subtotal', 'invoice_ids.currency_id')
+    # def compute_amount_total_currency(self):
+    #     company_currency = self.invoice_ids.company_id.currency_id
+    #     for l in self:
+    #         amount_total = l.currency_id.compute(l.price_subtotal, company_currency)
+    #         l.amount_total = amount_total
+
 class product_lines(models.Model):
     _name = 'product.lines'
     _description = 'product.lines'
-    # _order = 'order_id'
 
     name = fields.Char(string='Product')
     product_name = fields.Many2one(comodel_name='purchasing.stationery', string='Product Name')
     code_pr = fields.Many2one(comodel_name='purchasing.stationery', string='PR')
-    # code_product = fields.Many2one(comodel_name='purchasing.stationery', string='CP/')
     product_id = fields.Many2one(comodel_name='product.template', string='Name Product')
     date_planned = fields.Datetime(string='Request Date', index=True)
-
-    # order_id = fields.Many2one('purchasing.stationery', string='Order Reference', index=True, required=True, ondelete='cascade')
-
-    # price_subtotal = fields.Monetary(compute='_compute_amount', string='Subtotal', store=True)
-    # price_total = fields.Monetary(compute='_compute_amount', string='Total', store=True)
-    # price_tax = fields.Float(compute='_compute_amount', string='Tax', store=True)
 
  #  ---------------------  Autofill / Join field product_uom, product_qty, price_unit by product_id  -----------------------
     product_qty = fields.Float(string='Quantity', required=True)
     price_unit = fields.Many2one('res.currency', string='Unit Price', )
     product_uom = fields.Many2one('uom.uom', string='Unit Of Measure', default=lambda self: self.env['uom.uom'].search([]))
+
+    currency_id = fields.Many2one(store=True, string='Currency', readonly=True)
+    date_order = fields.Datetime(string='Order Date', readonly=True)
+    company_id = fields.Many2one('res.company', string='Company', store=True, readonly=True)
 
     _sql_constraints = [
         ('accountable_required_fields',
@@ -279,7 +189,6 @@ class product_lines(models.Model):
     @api.onchange('product_id')
     def _onchange_product_id(self):
         for rec in self:
-            # lines = [(5, 0, 0)]
             lines = []
             print("self.prduct_id", self.product_id.product_variant_ids)
             for line in self.product_id.product_variant_ids:
@@ -297,73 +206,10 @@ class product_lines(models.Model):
 class ProductInfo(models.Model):
     _name = 'purchasing.product_info'
 
-    # name = fields.Char(store="True")
-    # name = fields.Char(compute='_compute_name', store="True")
     info_product = fields.Char(string='Information')
-    # state = fields.Char(string='State', store=True)
     employee_id = fields.Many2one(comodel_name='res.partner', string='Employee')
     company_id = fields.Many2one(comodel_name='res.company', string='Company')
-
-    # @api.depends('info_product','state')
-    # def _compute_name(self):
-    #     for record in self:
-    #         record.name = str(record.info_product)+str(record.state)
-
-#     @api.model
-#     def create(self, vals):
-#         if vals.get('info_product', _('New')) == _('New'):
-#             vals['info_product'] = self.env['ir.sequence_info'].next_by_code('purchasing.product_info.sequence_info') or _('New')
-#         result = super(ProductInfo, self).create(vals)
-
-    # def onchange_info_state(self, cr, uid, ids, info_product, state, context=None):
-    #     v = {}
-    #     if info_product and state:
-    #         v['name'] = info_product+state
-    #     return {'value': v}
-
-    # @api.model
-    # def create(self, vals):
-    #     if vals['info_product']:
-    #         info_product = vals['info_product'].title()
-    #     else:
-    #         info_product = ''
-    #     if vals['state']:
-    #         state = vals['state'].title()
-    #     else:
-    #         state = ''
-    #         name = "{}, {}".format(state, info_product,)
-    #     print 'name', name
-    #         vals['name'] = name
-    #         vals['info_product'] = info_product
-    #         vals['state'] = state
-    #     if 'address_id' in vals:
-    #         res_id = super(HrEmployee, self).create(vals)
-    #     return res_id
-        # return
 
 # --------------------- Inherit Model Product -----------------------
 class ProductProduct(models.Model):
     _inherit = 'product.product'
-
-    # infoproduct = fields.Many2one('purchasing.product_info', string='Info Product')
-    # state_product = fields.Char(related='infoproduct.state_product', string='State Product')
-
-# class PurchaseOrder(models.Model):
-#     _name = "purchase.order"
-#     _inherit = ['mail.thread', 'mail.activity.mixin', 'portal.mixin']
-#     _description = "Purchase Order"
-#     _order = 'date_order desc, id desc'
-
-    # @api.depends('order_line.price_total')
-    # def _amount_all(self):
-    #     for order in self:
-    #         amount_untaxed = amount_tax = 0.0
-    #         for line in order.order_line:
-    #             line._compute_amount()
-    #             amount_untaxed += line.price_subtotal
-    #             amount_tax += line.price_tax
-    #         order.update({
-    #             'amount_untaxed': order.currency_id.round(amount_untaxed),
-    #             'amount_tax': order.currency_id.round(amount_tax),
-    #             'amount_total': amount_untaxed + amount_tax,
-    #         })
